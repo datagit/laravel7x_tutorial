2e591bdbe4ec5f728ddd5d3679b078487a5c458e
Dat Dao <datagit@yahoo.com>
2020-09-26 12:32:50 +0700

    elequent cursor method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,14 +38,19 @@ class PostController extends Controller
 //            }
 //        });
 
-        User::where('id', '>', 0)->chunk(200, function ($users) {
-            var_dump(count($users));
-            foreach ($users as $user) {
-                $name = (rand(0,1) == 1) ? $user->name . '_add1' : $user->name . '_add0';
-                // might be more logic here
-                $user->update(['name' => $name]);
-            }
-        });
+//        User::where('id', '>', 0)->chunk(200, function ($users) {
+//            var_dump(count($users));
+//            foreach ($users as $user) {
+//                $name = (rand(0,1) == 1) ? $user->name . '_add1' : $user->name . '_add0';
+//                // might be more logic here
+//                $user->update(['name' => $name]);
+//            }
+//        });
+
+        foreach (User::where('id', '>', 98)->cursor() as $user) {
+            var_dump($user->id);
+        }
+
 
         return view('post', [
             'post' => 'POST',
